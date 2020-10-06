0af4c78028e98c94970fe49e846fb1f0ff22db49
coderboy <study24forall@gmail.com>
2020-10-06 14:32:07 +0530

    hello.java file changed

---
+++ TestProject/src/gitDemo/hello.java
@@ -0,0 +1,6 @@
+public class hello{
+public static void main(String args[]){
+System.out.println("used git add ,git status ,commit and push");
+}
+//git add hello.java -- git status--git commit -git push
+}
