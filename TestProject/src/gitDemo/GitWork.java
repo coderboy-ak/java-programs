90e92835889aeb448b46e012803abc718a1a9b7b
amit <study24forall@gmail.com>
2020-10-06 12:54:13 +0530

    Added package gitDemo

--- TestProject/src/gitDemo/GitWork.java
+++ TestProject/src/gitDemo/GitWork.java
@@ -1,5 +1,16 @@
 package gitDemo;
 
 public class GitWork {
-
+public static void main(String[] args) {
+	System.out.println("How to add github project to eclipse: ");
+	System.out.println(" 1 .create a github account and signin");
+	System.out.println("2. start a project -create a repository ");
+	System.out.println("3. Start eclipse ");
+	System.out.println("4.Go to perspective-> git repository-> click on add git repository");
+	System.out.println("5. create a project in eclipse");
+	System.out.println("6. do a right click on project >  team > share >add to git repo ");
+	System.out.println("7.commit and push the project in repo ");
+	System.out.println("8.commit and push any change to the repository.");
+	
+}
 }
